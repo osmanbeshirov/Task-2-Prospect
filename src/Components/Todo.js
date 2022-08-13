864a6d7ede7878948082da3daa59fa0879673763
osmanbeshirov <osman.beshirov@gmail.com>
2022-08-14 02:13:35 +0400

    updated project

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 import '../styles/Form.css'
 import bin from '../photos/Vector.png'
+import enoughTodos from '../photos/Group.png'
 
 export default function Todo({ todos, handleRemoveAllTodos, removeCurrentTodo }) {
+
 
     const showStatus = () => {
 
@@ -19,32 +21,59 @@ export default function Todo({ todos, handleRemoveAllTodos, removeCurrentTodo })
         )
     }
 
+    const isNotEnoughTodos = () => {
+        return (
+            <div className='form-parent modifiaer'>
+
+                <div className='parent--modifaer' >
+                    <img src={enoughTodos} />
+
+                    <h2>Heç bir tapşırıq yoxdur</h2>
+
+                </div>
+
+
+                {showStatus()}
+
+            </div>
+        )
+    }
+
+    const haveTodos = () => {
+        return (
+            <div>
+                <div className='form-parent'>
+                    <form className='current--form'>
+                        <ul>
+                            {todos.map((todo, index) => (
+                                <li className='list-item' key={index}>
+                                    <div className='list-item__inputs'>
+                                        <input className='current-todo__checkbox' type='checkbox' />
+
+                                        <input readOnly type='text'
+                                            name='current_todo'
+                                            value={todo.text}
+                                            className='main-form__current-input'
+                                        />
+                                    </div>
+
+                                    <img onClick={() => removeCurrentTodo(todo)} className='remove-todo' src={bin} alt={bin} />
+                                </li>
+                            ))}
+
+                        </ul>
+
+                        {todos.length > 0 ? showStatus() : null}
+                    </form>
+                </div>
+            </div>
+        )
+    }
+
+
     return (
         <div>
-            <div className='form-parent'>
-                <form className='current--form'>
-                    <ul>
-                        {todos.map((todo, index) => (
-                            <li className='list-item' key={index}>
-                                <div className='list-item__inputs'>
-                                    <input className='current-todo__checkbox' type='checkbox' />
-
-                                    <input readOnly type='text'
-                                        name='current_todo'
-                                        value={todo.text}
-                                        className='main-form__current-input'
-                                    />
-                                </div>
-
-                                <img onClick={() => removeCurrentTodo(todo)} className='remove-todo' src={bin} alt={bin} />
-                            </li>
-                        ))}
-
-                    </ul>
-
-                    {todos.length > 0 ? showStatus() : null}
-                </form>
-            </div>
+            {todos.length == 0 ? isNotEnoughTodos() : haveTodos()}
         </div>
     )
 }
